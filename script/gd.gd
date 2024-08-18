cad0801af82b7c4f1042139e39a3af4db3372f2e
Jiahao Xiang <jiahaoxiang2000@gmail.com>
2024-08-18 15:49:37 +0800

    add the input action for the game

--- script/gd.gd
+++ script/gd.gd
@@ -8,8 +8,16 @@ func _init():
 
 
 func _process(delta):
-	rotation += angular_speed * delta
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
 
-	var velocity = Vector2.UP.rotated(rotation) * speed
+	rotation += angular_speed * direction * delta
+
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
 
 	position += velocity * delta
